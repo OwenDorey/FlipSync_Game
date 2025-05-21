e1ac370228a33ba7484be619a959bb7039e0af09
OwenDorey <123426282+OwenDorey@users.noreply.github.com>
2025-05-21 17:33:22 +0100

    Level Select Screen

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var scene_transition_animation: AnimationPlayer = $SceneTransitionAnimation/AnimationPlayer
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func load_scene(scene_name : String):
 	scene_transition_animation.play("fade_in")
@@ -9,10 +10,13 @@ func load_scene(scene_name : String):
 	get_tree().change_scene_to_file(scene_name)
 	
 func _on_play_button_button_down() -> void:
-	load_scene("res://Scenes/Levels/level_1.tscn")
+	load_scene("res://Scenes/Levels/level_5.tscn")
 
 func _on_quit_button_button_down() -> void:
 	scene_transition_animation.play("fade_in")
 	await get_tree().create_timer(0.5).timeout
 	set_physics_process(false)
 	get_tree().quit()
+
+func _on_level_select_button_button_down() -> void:
+	animation_player.play("open_level_select")

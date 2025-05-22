e28d83e144f8435e7a3dbc22e281851ab65fed84
OwenDorey <123426282+OwenDorey@users.noreply.github.com>
2025-05-22 16:59:26 +0100

    Finalised level select screen

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -2,6 +2,16 @@ extends Control
 
 @onready var scene_transition_animation: AnimationPlayer = $SceneTransitionAnimation/AnimationPlayer
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var level_buttons: GridContainer = $Levels/LevelButtons
+
+func _ready() -> void:
+	for i in range(1, 9):
+		var button = level_buttons.get_node("Button%d" % i)
+		button.pressed.connect(_on_level_button_pressed.bind(i))
+		
+func _on_level_button_pressed(level_index):
+	var path = "res://Scenes/Levels/level_%d.tscn" % level_index
+	load_scene(path)
 
 func load_scene(scene_name : String):
 	scene_transition_animation.play("fade_in")
@@ -10,7 +20,7 @@ func load_scene(scene_name : String):
 	get_tree().change_scene_to_file(scene_name)
 	
 func _on_play_button_button_down() -> void:
-	load_scene("res://Scenes/Levels/level_8.tscn")
+	load_scene("res://Scenes/Levels/level_1.tscn")
 
 func _on_quit_button_button_down() -> void:
 	scene_transition_animation.play("fade_in")
@@ -20,3 +30,6 @@ func _on_quit_button_button_down() -> void:
 
 func _on_level_select_button_button_down() -> void:
 	animation_player.play("open_level_select")
+
+func _on_back_button_button_down() -> void:
+	animation_player.play("open_menu")
